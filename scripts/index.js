33a68c002f88215066347b0fbdc063b57fb85a8a
Dima <fgjd.gh50@gmail.com>
2024-12-13 17:27:17 +0200

    upload

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+document.getElementById('myButton').addEventListener(onclick,() => {
+    window.location.href = "HomePage.html";
+})
+
+
+let myButton = document.getElementById('myButton');
+myButton.addEventListener('click', () => {
+    window.location.href = "HomePage.html";
+});"#07b8cc"
+if(myButton){
+    myButton.addEventListener('click', () => {
+        window.location.href = "HomePage.html";
+    });
+}
