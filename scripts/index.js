248ba5ca0c3632379a5010fa1554d86c8fc938dc
Dima <fgjd.gh50@gmail.com>
2024-12-14 13:18:31 +0200

    writing functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
+const tasks = document.getElementById('tasks');
+const addButton = document.getElementById('addButton');
+const myInput = document.getElementById('myInput');
+const homeButton = document.getElementById('homeButton');
+const userButton = document.getElementById('userButton');
+const shopButton = document.getElementById('shopButton');
+const healthButton = document.getElementById('healthButton');
+
+
+
 document.getElementById('myButton').addEventListener(onclick,() => {
     window.location.href = "HomePage.html";
 })
 
 
-let myButton = document.getElementById('myButton');
-myButton.addEventListener('click', () => {
-    window.location.href = "HomePage.html";
-});"#07b8cc"
-if(myButton){
-    myButton.addEventListener('click', () => {
-        window.location.href = "HomePage.html";
-    });
-}
+
